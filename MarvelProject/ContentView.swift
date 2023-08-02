241ecbb7f32004c76a4d0511efee1fd561abcd70
Ben.Clark <Ben.Clark@Gamesys.co.uk>
2023-08-02 11:31:03 +0100

    Initial Commit

---
+++ MarvelProject/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  MarvelProject
+//
+//  Created by Ben Clark on 02/08/2023.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ MarvelProject/MarvelProjectApp.swift
@@ -0,0 +1,17 @@
+//
+//  MarvelProjectApp.swift
+//  MarvelProject
+//
+//  Created by Ben Clark on 02/08/2023.
+//
+
+import SwiftUI
+
+@main
+struct MarvelProjectApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
